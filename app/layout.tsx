7e32b9db76e88696c54b3f4179081fb0e324070c
Paras <paras.r@squareinfosoft.com>
2026-01-16 19:14:28 +0530

    minor changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Roboto } from "next/font/google";
 import "./globals.css";
 import { SidebarContextProvider } from "@/src/context/sidebar.context";
 import IndexLayout from "@/src/indexLayout/indexLayout";
-import { AuthProvider } from "@/src/context/auth.context";
 
 const roboto = Roboto({
   weight: ["100", "300", "400", "500", "700"],
@@ -25,13 +24,11 @@ export default function RootLayout({
       <body
         className={`${roboto.className} antialiased`}
       >
-        <AuthProvider>
-          <SidebarContextProvider>
-            <IndexLayout>
-              {children}
-            </IndexLayout>
-          </SidebarContextProvider>
-        </AuthProvider>
+        <SidebarContextProvider>
+          <IndexLayout>
+            {children}
+          </IndexLayout>
+        </SidebarContextProvider>
       </body>
     </html>
   );
